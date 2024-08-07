3dde3b5b5aa5e3bdce7378e741dc64ec40c626db
Roshni Rai <154725729+rairoshni2005@users.noreply.github.com>
2024-08-07 08:24:13 +0530

    Add files via upload

---
+++ number_positive_negative_zero.java
@@ -0,0 +1,23 @@
+//WAP to find whether number is positive, negative or zero.
+
+import java.util.Scanner;
+
+public class number_positive_negative_zero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        scanner.close();
+    }
+}
+
